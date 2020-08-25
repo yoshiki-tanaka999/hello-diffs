82b2edbda571203a9137e4a5a1b311848257ca7e
yoshiki tanaka <shootingstarorufo@gmail.com>
2020-08-25 09:36:15 +0900

    vuexインストール
    npm install --save-dev vuex
    vuex@3.5.1
    とりあえず、インストールの履歴を残す

---
+++ resources/js/store/index.js
@@ -0,0 +1,17 @@
+// ディスカッションページ用のstoreの作成
+
+const state = {}
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
