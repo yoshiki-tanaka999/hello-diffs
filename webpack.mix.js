12d96232894da63d64c2e3072a244474ba548db5
yoshiki tanaka <shootingstarorufo@gmail.com>
2020-09-19 16:54:44 +0900

    webpack.mix.js編集

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// 追加
+mix.browserSync('vuesplash.test')
+.js('resources/js/app.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css')
+.version()
