ea65d07eda239cc4282cb8066e07aca7b0e1963d
yoshiki tanaka <shootingstarorufo@gmail.com>
2020-09-19 19:04:04 +0900

    Vuexの設定

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import post from './post'
+import store from './store'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     modules: {
-        post,
+        post, auth
     }
 })
 
