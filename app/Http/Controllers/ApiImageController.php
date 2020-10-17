f16dce74289de3581a8f4da40389114f4b69b24c
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-10-17 12:52:20 +0900

    読み込み

--- app/Http/Controllers/ApiImageController.php
+++ app/Http/Controllers/ApiImageController.php
@@ -58,13 +58,15 @@ class ApiImageController extends Controller
         // }
 
         // S3導入後のコード
-        if (request()->file) {
-            $file_name = time() . '.' . request()->file->getClientOriginalName();
-            request()->file('file');
-            $path = Storage::disk('s3')->put('/img_url', $file_name, 'public');
+        if ($request->hasFile('datafile')) {
+            // $file_name = time() . '.' . request()->file->getClientOriginalName();
+            // request()->file('file');
+            // $path = Storage::disk('s3')->put('/img_url', $file_name, 'public');
             // $path = Storage::disk('s3')->put('/img_url', $file, 'public');
+
+            $path = Storage::disk('s3');
             // S3にファイルを保存し、保存したファイル名を取得する
-            // $fileName = $path->put('', $request->file('datafile'));
+            $fileName = $path->put('', $request->file('datafile'));
 
             $user = Auth::user();
 
