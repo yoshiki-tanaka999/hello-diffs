c3d549d5d818e17fd07e685f5bdfa0ed8461defd
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-10-27 17:42:26 +0900

    thisが要らない？

--- resources/js/components/ClaimTab2Act2Component.vue
+++ resources/js/components/ClaimTab2Act2Component.vue
@@ -82,7 +82,7 @@
                             height="85"
                             class="mx-auto my-4"
                         >
-                            <router-link :to="{name: 'ClaimLayerviewAct2', params: {claimId : this.claim.content }}" >
+                            <router-link :to="{name: 'ClaimLayerviewAct2', params: {claimId : claim.content }}" >
 
                             <!-- データベースからテキストを描画 -->
                                 <v-card-text
