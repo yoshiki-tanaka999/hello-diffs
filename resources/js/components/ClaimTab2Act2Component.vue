6692a881467bd0c09d108f1d30bca0c8a7f49b87
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-10-27 11:32:23 +0900

    ClaimProsコンポーネント化

--- resources/js/components/ClaimTab2Act2Component.vue
+++ resources/js/components/ClaimTab2Act2Component.vue
@@ -1,12 +1,9 @@
 // Act２
 <template>
     <v-app class="whole" color="basil">
-
-
         <v-container 
             class="text-center justify-center py-6" 
         >
-
                 <!-- 論点に対する主張 -->
             <v-card 
                 class="d-flex mx-auto"
@@ -74,46 +71,14 @@
                     max-width="600"
                 >
                 <!-- カード①賛成用 -->
-                    <template>
-                        <!-- <div> -->
-                            <v-card
-                                color="#C62828"
-                                dark
-                                width="600"
-                                max-height="200"
-                                height="85"
-                                class="mx-auto my-4"
-                            >
-                            <!-- v-ifで賛成、反対、その他ごとに紐付ける（それぞれ色を変えたい） -->
+                <ClaimsPros-component :id="id"></ClaimsPros-component>
 
-                            <!-- データベースからテキストを描画 -->
-                                <v-card-text
-                                    class="font-weight-black"
-                                    color="white"
-                                >
-                                    {{claim.content}}
-                                </v-card-text>
-                                <!-- <v-card-text>賛成です。</v-card-text> -->
-
-                                <!-- アイコンを追加 -->
-                                <div class="claimOutputValue">
-                                    <div class="postStatusList d-flex">
-                                        <!-- 「コメント数」 -->
-                                        <div><i class="far fa-comments mr-2 ml-3"></i>3</div>
-                                        <!-- 「参加者数」 -->
-                                        <div><i class="fas fa-users mr-2 ml-3"></i>2</div>
-                                        <!-- 「ブックマークされた数」 -->
-                                        <div><i class="fas fa-heart mr-2 ml-3"></i>1</div>
-                                    </div>
-                                </div>                                
-                            </v-card>
-                        <!-- </div> -->
-                    </template>  
                 </v-tab-item>
 
                 <v-tab-item value="cons"
                     v-if=" claim.claim_flag === '反対' "
                 >
+                <!-- <ClaimsCons-component :id="id"></ClaimsCons-component> -->
                 <!-- カード②反対用 -->
                 <!-- <div v-for=" in claim_output"> -->
                     <template>                        
---
+++ resources/js/components/ClaimsProsComponent.vue
@@ -0,0 +1,67 @@
+<template>
+    <!-- <div> -->
+        <v-card
+            color="#C62828"
+            dark
+            width="600"
+            max-height="200"
+            height="85"
+            class="mx-auto my-4"
+        >
+        <!-- v-ifで賛成、反対、その他ごとに紐付ける（それぞれ色を変えたい） -->
+
+        <!-- データベースからテキストを描画 -->
+            <v-card-text
+                class="font-weight-black"
+                color="white"
+            >
+                {{claim.content}}
+            </v-card-text>
+            <!-- <v-card-text>賛成です。</v-card-text> -->
+
+            <!-- アイコンを追加 -->
+            <div class="claimOutputValue">
+                <div class="postStatusList d-flex">
+                    <!-- 「コメント数」 -->
+                    <div><i class="far fa-comments mr-2 ml-3"></i>3</div>
+                    <!-- 「参加者数」 -->
+                    <div><i class="fas fa-users mr-2 ml-3"></i>2</div>
+                    <!-- 「ブックマークされた数」 -->
+                    <div><i class="fas fa-heart mr-2 ml-3"></i>1</div>
+                </div>
+            </div>                                
+        </v-card>
+    <!-- </div> -->
+</template> 
+
+<script>
+export default {
+    props: {
+        id: Number,
+        // claimId: Number,
+    },
+    data () {
+        return {
+            claimId: Number,
+            claims: [],
+            claim: [],
+        }
+    },
+    methods: {
+        getClaim() {
+            axios.get('/api/claim')
+            .then((res) => {
+                this.claim = res.data;
+                // this.claimId = this.claims[index].id
+                console.log(this.claim);  
+                // その他・補足のデータ
+            })
+        },
+    },
+    mounted() {
+        // this.getPost();
+        this.getClaim();
+    },
+}
+
+</script>  
